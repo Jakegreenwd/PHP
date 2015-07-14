eacee12487b607b78d770eb9a9408eeb2e42e0c5
Jake Greenwood <j@jakegreenwood.com>
2015-07-13 21:37:33 -0400

    PHP

---
+++ date.php
@@ -0,0 +1,34 @@
+<?php
+
+//Define variables
+$day = 'Monday';
+$date = '2';
+$year = 2015;
+$suffix = "";
+
+//switch statement to determine proper suffix to use after date variable
+switch($date) {
+    case 1:
+        $suffix = "st.";
+        break;
+    case 2:
+        $suffix = "nd.";
+        break;
+    case 3:
+        $suffix = "rd.";
+        break;
+    case 4-30:
+        $suffix = "th.";
+        break;
+    default:
+        $suffix = "th.";
+        break;
+}
+
+//echo string
+echo "The date is $day the $date$suffix, $year"
+
+?>
+
+
+
